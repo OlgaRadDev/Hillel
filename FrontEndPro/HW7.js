dec913c6bdd17e668f6acac39f1271836c7f2481
OlgaRadDev <olga.rad.dev@gmail.com>
2019-07-04 22:12:02 +0300

    Initial commit

---
+++ FrontEndPro/HW7.js
@@ -0,0 +1,10 @@
+var a = "";
+var b = 7;
+var c = null;
+var d = 2;
+var e;
+
+
+alert( "" || null && b || d );
+alert( (undefined  ||  null) && b &&  d );
+alert( undefined  ||  null || b || undefined && d);
