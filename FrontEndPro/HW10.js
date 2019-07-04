d76accc0ddbf835b0359f38edc7d8c209dc5cfa3
OlgaRadDev <olga.rad.dev@gmail.com>
2019-07-05 00:48:41 +0300

    Initial commit

---
+++ FrontEndPro/HW10.js
@@ -0,0 +1,42 @@
+// var number = +prompt("Enter a number:");
+// var power = +prompt("Enter power:");
+// var count = 1;
+// var res = 1;
+// while (count<=power) {
+//    res*=number;
+//    count++;
+// }
+// alert('Result: '+res);
+
+
+// var number = +prompt("Enter a number:");
+// var power = +prompt("Enter power:");
+// var count = 1;
+// var res = 1;
+//
+// do {
+//     res*=number;
+//     count++;
+// } while (count<=power)
+// alert('Result: '+res);
+
+
+var number = +prompt("Enter a number:");
+var power = +prompt("Enter power:");
+var res = 1;
+for (var i=0; i<power; i++) {
+    res*=number;
+}
+alert('Result: '+res);
+
+
+
+
+
+
+
+
+
+
+
+
