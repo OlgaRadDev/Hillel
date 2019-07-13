eb9b60b7d36edb9cf4783c389dd0204be1b70e8b
OlgaRadDev <olga.rad.dev@gmail.com>
2019-07-13 23:20:20 +0300

    Initial commit

--- FrontEndPro/HW15.js
+++ FrontEndPro/HW15.js
@@ -1,11 +1,12 @@
 function getNumberOfEven(n) {
     var count = 0;
 
-    for (var i = 0; i < arguments.length; ++i) {
-        if (arguments[i] % 2 === 0 && arguments[i] !== 0) {
+    while (n>0)
+    {
+        if ((n % 10) % 2 === 0) {
             count++;
         }
-
+        n = Math.floor(n/10);
     }
     return count;
 }
