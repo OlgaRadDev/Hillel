fcd182392d9623923deb0e232b752bd12ee6effa
OlgaRadDev <olga.rad.dev@gmail.com>
2019-07-15 00:25:53 +0300

    Initial commit

--- FrontEndPro/HW13.js
+++ FrontEndPro/HW13.js
@@ -3,8 +3,7 @@ var str = prompt('What is your full name');
 function someFunction (str) {
     var parts = str.split(' ');
     for (var i = 0; i <parts.length; i++) {
-        var j = parts[i].charAt(0).toUpperCase();
-        parts[i] = j + parts[i].substr(1).toLowerCase();
+        parts[i] = parts[i].charAt(0).toUpperCase() + parts[i].substr(1).toLowerCase();
     }
     return parts.join(' ');
 }
