77fd8f20d4da0fb71b4591d9d3ddbb3b417d774f
OlgaRadDev <olga.rad.dev@gmail.com>
2019-07-22 00:02:27 +0300

    Initial commit

--- FrontEndPro/HW19.js
+++ FrontEndPro/HW19.js
@@ -17,7 +17,7 @@
 // получение отсортированного по среднему балу списка студентов
 
 
-function createStudent (name,age) {
+function createStudent(name, age) {
 
     var obj = {
         name: name,
@@ -34,35 +34,74 @@ function manageStudents(students) {
             students[students.length] = createStudent(name, age);
         },
         deleteStudentByName: function (name) {
-           var index = students.findIndex(obj => obj.name === name);
-           // console.log(index)
-            students.splice(index,1);
+            for (var i = 0; i<students.length; i++) {
+                if (students[i].name === name) {
+                    students.splice(i, 1);
+                }
+            }
+            // var index = students.findIndex(obj => obj.name === name);
+            // // console.log(index)
+            // students.splice(index, 1);
+        },
+        markForLesson: function (name, number, value) {
+            for (var i = 0; i < students.length; i++) {
+                if (students[i].name === name) {
+                    students[i].marks[number - 1] = value;
+                }
+            }
+        },
+        averageMarkStudent: function (name) {
+            var sum = 0;
+            var count = 0;
+            var average;
 
-            // for (){
-            //    if(obj.name === name){
-            //        students.splice(i,1)
-            //    }
-            // }
-            
+            for (var i = 0; i < students.length; i++) {
+                if (students[i].name === name) {
+                    for (var j = 0; j < students[i].marks.length; j++) {
+                        sum += students[i].marks[j];
+                        count++;
+                    }
+                }
+            }
+            average = sum / count;
+            return average;
         },
-        markForLesson: function (value) {
+        averageMarkGroupForLesson: function (students, number) {
+            var sum = 0;
+            var count = 0;
+            var average;
+            for (var i = 0; i < students.length; i++) {
+                sum+=students[i].marks[number-1];
+                count++;
+            }
+            average = sum / count;
+            return average;
+        },
 
-            
+        sortListByNameStudents: function (students) {
+            students.sort(function (a, b) {
+                var nameA = a.name.toLowerCase();
+                var nameB = b.name.toLowerCase();
+                if (nameA < nameB)
+                    return -1;
+                if (nameA > nameB)
+                    return 1;
+                return 0;
+            });
         },
-        averageMarkStudent: function () {
-            
-        },
-        averageMarkGroupForLesson:function () {
-            
-        },
-        sortListByNameStudents: function () {
-            
-        },
-        sortListByAverageMark: function () {
-            
+        sortListByAverageMark: function (students) {
+            var manager = this;
+            students.sort(function (a, b) {
+                var firstStudentAVG = manager.averageMarkStudent(a.name);
+                var secondStudentAVG = manager.averageMarkStudent(b.name);
+                if (firstStudentAVG < secondStudentAVG)
+                    return 1;
+                if (firstStudentAVG > secondStudentAVG)
+                    return -1;
+                return 0;
+            });
         }
     }
-    
 }
 
 var studentsArr = [];
@@ -74,13 +113,59 @@ studentManager.addNewStudents('Mark', 21);
 studentManager.addNewStudents('Eva', 22);
 studentManager.addNewStudents('Andrew', 19);
 studentManager.addNewStudents('Sem', 23);
-
-console.log(studentsArr);
-studentManager.deleteStudentByName("Anna")
+console.log('Добавления нового студента')
 console.log(studentsArr);
 
-//
-// studentsArr[0] = createStudent('Alex', 23);
-// studentsArr[1] = createStudent('Ann', 21);
-// studentsArr[2] = createStudent('Mark', 20);
+console.log('Удаление студента по имени')
+studentManager.deleteStudentByName("Sem")
+console.log(studentsArr);
+
+// console.log('Добавление оценки студенту за занятие(№ занятия === индекс оценки в массиве)')
+studentManager.markForLesson('Alex', 1, 10);
+studentManager.markForLesson('Alex', 4, 5);
+studentManager.markForLesson('Alex', 2, 11);
+studentManager.markForLesson('Alex', 3, 7);
+studentManager.markForLesson('Alex', 5, 8);
+
+studentManager.markForLesson('Mark', 1, 6);
+studentManager.markForLesson('Mark', 4, 7);
+studentManager.markForLesson('Mark', 2, 12);
+studentManager.markForLesson('Mark', 3, 5);
+studentManager.markForLesson('Mark', 5, 9);
+
+studentManager.markForLesson('Anna', 1, 6);
+studentManager.markForLesson('Anna', 4, 7);
+studentManager.markForLesson('Anna', 2, 2);
+studentManager.markForLesson('Anna', 3, 5);
+studentManager.markForLesson('Anna', 5, 9);
+
+studentManager.markForLesson('Eva', 1, 6);
+studentManager.markForLesson('Eva', 4, 7);
+studentManager.markForLesson('Eva', 2, 2);
+studentManager.markForLesson('Eva', 3, 10);
+studentManager.markForLesson('Eva', 5, 9);
+
+studentManager.markForLesson('Andrew', 1, 6);
+studentManager.markForLesson('Andrew', 4, 7);
+studentManager.markForLesson('Andrew', 2, 11);
+studentManager.markForLesson('Andrew', 3, 10);
+studentManager.markForLesson('Andrew', 5, 9);
+
+console.log('Получение средней оценки студента по имени')
+var avg = studentManager.averageMarkStudent('Alex');
+console.log(avg);
+
+console.log('Получение средней оценки группы за занятие')
+var avgGroup =studentManager.averageMarkGroupForLesson(studentsArr, 2);
+console.log(avgGroup);
+
+console.log('Получение отсортированного по именам списка студентов')
+studentManager.sortListByNameStudents(studentsArr);
+console.log(studentsArr);
+
+console.log('Получение отсортированного по среднему балу списка студентов')
+studentManager.sortListByAverageMark(studentsArr);
+console.log(studentsArr);
+
+
 
